2452cfb19d6c04e85c50b2c4f9270d071249cf23
Suvarnasrayshi <suvarnasrayshi.sinha.2024@gmail.com>
2024-06-24 19:09:13 +0530

    v

--- formvalid.vue
+++ formvalid.vue
@@ -1,61 +1,76 @@
 <template>
   <div class="root">
-    <h2>form</h2>
+    <h2>Form</h2>
     <p>
-      <input type="text" name="email" id="email" placeholder="email" v-model="formData.email" @blur="v$.email.$touch">
-      <span v-if="v$.email.$error">email is required</span>
+      <input type="text" name="email" id="email" placeholder="Email" v-model="formData.email" @blur="v$.email.$touch()" />
+      <span v-if="v$.email.$error">Email is required and must be valid</span>
     </p>
     <p>
-      <input type="text" name="password" id="password" placeholder="password"  v-model="formData.password"  @blur="v$.password.$touch">
-      <span v-if="v$.password.$error">password is not proper</span>
+      <input type="text" name="password" id="password" placeholder="Password" v-model="formData.password"
+        @blur="v$.password.$touch()" />
+      <span v-if="v$.password.$error">Password is required and must be at least 2 characters</span>
     </p>
     <p>
-      <input type="text" name="confirm" id="confirm" placeholder="confirm password"  v-model="formData.confirm" @blur="v$.confirm.$touch">
-      <span v-if="v$.confirm.$error">required</span>
+      <input type="text" name="confirm" id="confirm" placeholder="Confirm Password" v-model="formData.confirm"
+        @blur="v$.confirm.$touch()" />
+      <span v-if="v$.confirm.$error">Confirmation is required</span>
     </p>
     <p>
       True
-      <input type="radio" name="bool" v-model="bool" :value="true" @blur="v$.bool.$touch"/>
-  
+      <input type="radio" name="bool" v-model="formData.bool" :value="true" @blur="v$.bool.$touch()" />
       False
-      <input type="radio" name="bool" v-model="bool" :value="false" @blur="v$.bool.$touch"/>
-      <span v-if="v$.bool.$error && v$.bool.$dirty">check is not proper</span>
-      <pre>{{ $v }}</pre>
+      <input type="radio" name="bool" v-model="formData.bool" :value="false" @blur="v$.bool.$touch()" />
+      <span v-if="v$.bool.$error && v$.bool.$dirty">Selection is required</span>
     </p>
-    <button @click="submitdata">submit</button>
+
+    <p>
+      <label for="cars">Choose a car:</label>
+
+<select name="cars" id="cars" v-model="formData.car" @blur="v$.car.$touch()">
+  <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+</select>
+<sapn v-if="v$.car.$error">Seelct any one here</sapn>
+{{ formData.car }}
+    </p>
+    <button @click="submitdata">Submit</button>
   </div>
-  </template>
-  <script setup> 
-  import { useVuelidate } from '@vuelidate/core'
-  import { required,email,minLength } from '@vuelidate/validators'
-  import { reactive } from 'vue' 
-  
-  
-  
-  const formData = reactive({
-       email:"",
-       password:"",
-       confirm:"",
-       bool:"",
-      })
-  const rules={
-    email:{ required,email },
-    password:{ required ,minLength:minLength(2)},
-    // confirm:{ required,sameAs:sameAs(formData.password) },
-    confirm:{required},
-    bool:{required}
+</template>
+
+<script setup>
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
+import { reactive } from 'vue'
+
+const formData = reactive({
+  email: '',
+  password: '',
+  confirm: '',
+  bool: '',
+  car:'',
+})
+
+const rules = {
+  email: { required, email },
+  password: { required, minLength: minLength(2) },
+  confirm: { required },
+  bool: { required },
+  car: { required },
+}
+
+const v$ = useVuelidate(rules, formData)
+
+const submitdata = async () => {
+  console.log('formdatapassword', formData.password)
+  console.log('confirm', formData.confirm)
+  const result = await v$.value.$validate()
+  if (result) {
+    alert('Success')
+  } else {
+    alert('Error')
   }
-  const submitdata = async () => {
-  console.log("formdatapassword",formData.password)
-  console.log("confirm",formData.password);
-    console.log(v$.value);
-   const result=await v$.value.$validate();
-   if(result){alert("success")}
-   else{
-    alert("error")
-   }
-  }
-  
-  const v$ = useVuelidate(rules, formData)
-  </script>
-  
+}
+</script>
+
