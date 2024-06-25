978f4caba6117b0115d7eac187fc1c188a8992cb
Suvarnasrayshi <suvarnasrayshi.sinha.2024@gmail.com>
2024-06-25 11:43:00 +0530

    f

--- formvalid.vue
+++ formvalid.vue
@@ -6,14 +6,14 @@
       <span v-if="v$.email.$error">Email is required and must be valid</span>
     </p>
     <p>
-      <input type="text" name="password" id="password" placeholder="Password" v-model="formData.password"
+      <input type="password" name="password" id="password" placeholder="Password" v-model="formData.password"
         @blur="v$.password.$touch()" />
       <span v-if="v$.password.$error">Password is required and must be at least 2 characters</span>
     </p>
     <p>
-      <input type="text" name="confirm" id="confirm" placeholder="Confirm Password" v-model="formData.confirm"
+      <input type="password" name="confirm" id="confirm" placeholder="Confirm Password" v-model="formData.confirm"
         @blur="v$.confirm.$touch()" />
-      <span v-if="v$.confirm.$error">Confirmation is required</span>
+      <span v-if="v$.confirm.$error">Confirmation is required and must match the password</span>
     </p>
     <p>
       True
@@ -22,18 +22,22 @@
       <input type="radio" name="bool" v-model="formData.bool" :value="false" @blur="v$.bool.$touch()" />
       <span v-if="v$.bool.$error && v$.bool.$dirty">Selection is required</span>
     </p>
-
     <p>
       <label for="cars">Choose a car:</label>
-
-<select name="cars" id="cars" v-model="formData.car" @blur="v$.car.$touch()">
-  <option value="volvo">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="mercedes">Mercedes</option>
-  <option value="audi">Audi</option>
-</select>
-<sapn v-if="v$.car.$error">Seelct any one here</sapn>
-{{ formData.car }}
+      <select name="cars" id="cars" v-model="formData.car" @blur="v$.car.$touch()">
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
+      <span v-if="v$.car.$error">Select a car</span>
+    </p>
+    <p>
+      <label>Select Hobbies:</label><br />
+      Reading <input type="checkbox" value="reading" v-model="formData.hobbies" @blur="v$.hobbies.$touch()" />
+      Traveling <input type="checkbox" value="traveling" v-model="formData.hobbies" @blur="v$.hobbies.$touch()" />
+      Sports <input type="checkbox" value="sports" v-model="formData.hobbies" @blur="v$.hobbies.$touch()" />
+      <span v-if="v$.hobbies.$error">Select at least one hobby</span>
     </p>
     <button @click="submitdata">Submit</button>
   </div>
@@ -41,36 +45,47 @@
 
 <script setup>
 import { useVuelidate } from '@vuelidate/core'
-import { required, email, minLength } from '@vuelidate/validators'
+import { required, email, minLength, helpers } from '@vuelidate/validators'
 import { reactive } from 'vue'
 
 const formData = reactive({
   email: '',
   password: '',
   confirm: '',
-  bool: '',
-  car:'',
+  bool: null,
+  car: '',
+  hobbies: []
 })
 
+const mustbeemail =(value)=>value.includes('learn')
+
 const rules = {
-  email: { required, email },
+  email: { required, email,mustbeemail:helpers.withMessage('hello',mustbeemail) },
   password: { required, minLength: minLength(2) },
-  confirm: { required },
+  confirm: { required},
   bool: { required },
   car: { required },
+  hobbies: { required }
 }
 
 const v$ = useVuelidate(rules, formData)
 
 const submitdata = async () => {
-  console.log('formdatapassword', formData.password)
-  console.log('confirm', formData.confirm)
-  const result = await v$.value.$validate()
-  if (result) {
-    alert('Success')
-  } else {
-    alert('Error')
-  }
+  // const result =
+   await v$.value.$validate()
+  // if (result) {
+  //   alert('Success')
+  // } else {
+  //   alert('Error')
+  // }
 }
 </script>
 
+<style scoped>
+span {
+  color: red;
+}
+button {
+  margin-top: 10px;
+}
+</style>
